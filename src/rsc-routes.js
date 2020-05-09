07d4b9df4f36b0e044ff3953d9eaba7a8f4c388e
MusicDev33 <smccowan33@gmail.com>
2020-05-09 12:58:54 -0600

    Index fixes and separation

--- src/rsc-routes.js
+++ src/rsc-routes.js
@@ -3,19 +3,21 @@ const router = express.Router();
 const path = require('path');
 let baseUrl = require('./config/appcfg').baseUrl;
 
-baseUrl = baseUrl + 'assets/'
+assetsBaseUrl = baseUrl + 'assets/'
 
 const baseFilePath = __dirname + '/../assets/'
 
 module.exports = (app) => {
-  app.use(baseUrl + 'api/', express.static(baseFilePath + 'api'));
-  app.use(baseUrl + 'css/', express.static(baseFilePath + 'css'));
-  app.use(baseUrl + 'fonts', express.static(baseFilePath + 'fonts'));
-  app.use(baseUrl + 'img/', express.static(baseFilePath + 'img'));
-  app.use(baseUrl + 'js/', express.static(baseFilePath + 'js'));
-  app.use(baseUrl + 'kbl-css/', express.static(baseFilePath + 'kbl-css'));
-  app.use(baseUrl + 'kbl-img/', express.static(baseFilePath + 'kbl-img'));
-  app.use(baseUrl + 'tools/', express.static(baseFilePath + 'tools'));
+  app.use(assetsBaseUrl + 'api/', express.static(baseFilePath + 'api'));
+  app.use(assetsBaseUrl + 'css/', express.static(baseFilePath + 'css'));
+  app.use(assetsBaseUrl + 'fonts', express.static(baseFilePath + 'fonts'));
+  app.use(assetsBaseUrl + 'img/', express.static(baseFilePath + 'img'));
+  app.use(assetsBaseUrl + 'js/', express.static(baseFilePath + 'js'));
+  app.use(assetsBaseUrl + 'kbl-css/', express.static(baseFilePath + 'kbl-css'));
+  app.use(assetsBaseUrl + 'kbl-img/', express.static(baseFilePath + 'kbl-img'));
+  app.use(assetsBaseUrl + 'tools/', express.static(baseFilePath + 'tools'));
+
+  app.use(baseUrl + 'pages/', express.static(__dirname + '/../pages'));
 
   // app.use(baseUrl + 'public/css', express.static(path.join(__dirname, '/../public/css')));
 }
