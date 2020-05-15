47f9a309e76c40859612d08dda276a2d850b5363
MusicDev33 <smccowan33@gmail.com>
2020-05-15 13:24:36 -0600

    Oops

---
+++ src/rsc-routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+let baseUrl = require('./config/appcfg').baseUrl;
+
+assetsBaseUrl = baseUrl + 'assets/'
+
+const baseFilePath = __dirname + '/../assets/'
+
+module.exports = (app) => {
+  app.use(assetsBaseUrl + 'api/', express.static(baseFilePath + 'api'));
+  app.use(assetsBaseUrl + 'css/', express.static(baseFilePath + 'css'));
+  app.use(assetsBaseUrl + 'fonts', express.static(baseFilePath + 'fonts'));
+  app.use(assetsBaseUrl + 'img/', express.static(baseFilePath + 'img'));
+  app.use(assetsBaseUrl + 'js/', express.static(baseFilePath + 'js'));
+  app.use(assetsBaseUrl + 'kbl-css/', express.static(baseFilePath + 'kbl-css'));
+  app.use(assetsBaseUrl + 'kbl-img/', express.static(baseFilePath + 'kbl-img'));
+  app.use(assetsBaseUrl + 'tools/', express.static(baseFilePath + 'tools'));
+
+  app.use(baseUrl + 'pages/', express.static(__dirname + '/../pages'));
+
+  // app.use(baseUrl + 'public/css', express.static(path.join(__dirname, '/../public/css')));
+}
