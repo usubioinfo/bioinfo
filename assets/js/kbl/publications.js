644b5b0546b5f3a80ccf819e02805aaed2535c96
MusicDev33 <smccowan33@gmail.com>
2021-06-08 15:08:43 -0600

    Conferences now works on publications page

--- assets/js/kbl/publications.js
+++ assets/js/kbl/publications.js
@@ -1,8 +1,14 @@
 (() => {
   const pubDateButtons = [...document.getElementsByClassName('pub')];
+  const confDateButtons = [...document.getElementsByClassName('conf')];
+
   const pubGroups = [...document.getElementsByClassName('pub-group')];
+  const confGroups = [...document.getElementsByClassName('conf-group')];
 
   pubDateButtons[0].classList.add('active');
+  confDateButtons[0].classList.add('active');
+
+  confGroups[0].classList.remove('d-none');
 
   for (let pubButton of pubDateButtons) {
     pubButton.addEventListener('click', (e) => {
@@ -16,6 +22,21 @@
 
       pubButton.classList.add('active');
       document.getElementById(`pub-${pubButton.id}`).classList.remove('d-none');
+    });
+  }
+
+  for (let confButton of confDateButtons) {
+    confButton.addEventListener('click', (e) => {
+      for (let button of confDateButtons) {
+        button.classList.remove('active');
+      }
+
+      for (let group of confGroups) {
+        group.classList.add('d-none');
+      }
+
+      confButton.classList.add('active');
+      document.getElementById(`conf-${confButton.id}`).classList.remove('d-none');
     });
   }
 })();
