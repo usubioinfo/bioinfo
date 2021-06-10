80e413a60671b17674341ca77825b48fcc0aa4e8
MusicDev33 <smccowan33@gmail.com>
2021-06-10 11:50:29 -0600

    Remove unused JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,18 +10,7 @@ jQuery(document).ready(function($) {
 	"use strict";
 
 	var sequenceElement_single = document.getElementById("sequence-single");
-	$('.sabbi-book_timeline .btn-expand').on('click', function(event) {
-		event.preventDefault();
-		var btnThis = $(this);
-		btnThis.siblings('.book-list').find('.onexpan').slideToggle(200);
-		
-		if (btnThis.text() != "View Less" ) {
-			btnThis.text('View Less');
-		} else {
-			btnThis.text(btnThis.data('text'));
-		}
-	});
-	
+
 	// Place your Sequence options here to override defaults
 	// See: http://sequencejs.com/documentation/#options
 	var options = {
@@ -38,12 +27,12 @@ jQuery(document).ready(function($) {
 		var mySequence_single = sequence(sequenceElement_single, options);
 	}
 	// Launch Sequence on the element, and with the options we specified above
-	
-	
+
+
 	(function(){
 		var sequenceElement = document.getElementById("sequence");
 		//console.log(sequenceElement)
-		
+
 		// Place your Sequence options here to override defaults
 		// See: http://sequencejs.com/documentation/#options
 		var options = {
@@ -56,10 +45,6 @@ jQuery(document).ready(function($) {
 		  keyNavigation: true,
 		  fadeStepWhenSkipped: false
 		}
-		if($(sequenceElement).length) {
-		// Launch Sequence on the element, and with the options we specified above
-			var mySequence = sequence(sequenceElement, options);
-		}
 	})();
 
 
@@ -67,69 +52,4 @@ jQuery(document).ready(function($) {
 	    event.preventDefault();
 	    $(this).ekkoLightbox();
 	});
-	if ($("#Inputphone").length) {
-		$("#Inputphone").intlTelInput({
-	      // allowDropdown: false,
-	      // autoHideDialCode: false,
-	      // autoPlaceholder: "off",
-	      // dropdownContainer: "body",
-	      // excludeCountries: ["us"],
-	      // formatOnDisplay: false,
-	      geoIpLookup: function(callback) {
-	        $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-	          var countryCode = (resp && resp.country) ? resp.country : "";
-	          callback(countryCode);
-	        });
-	      },
-	      // initialCountry: "auto",
-	      // nationalMode: false,
-	      // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
-	      // placeholderNumberType: "MOBILE",
-	      preferredCountries: ['us'],
-	      // separateDialCode: true,
-	      utilsScript: "assets/tools/intlTelInput/js/utils.js"
-	    });
-	}
-
-	// Function for email address validation and contact form ajax code
-	function validateEmail(email) {
-	    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-	    return re.test(email);
-	}
-	/******************** CONTACT FORM ********************/
-	$('#contact-form').on('submit', function(e) {
-		//console.log($(this).attr('action'));
-		var formThat = $(this);
-	    e.preventDefault();
-	    $('#form_errors').addClass('hide');
-	    var data = $(this).serialize();
-	    
-	    if (validateEmail( $(this).find('input[name="email"]').val() )) {
-	        $.ajax({
-	            type: "POST",
-	            url: $(this).attr('action'),
-	            data: data,
-	            success: function(res) {
-	                //console.log(res);
-	                res = $.parseJSON(res);
-	                //console.log(formThat.text())
-	                if(res.status) {
-	                	$('#form_errors').addClass('hide');
-	                    $('#form_success').html(res.message).removeClass('hide');
-	                    formThat.find('.btn').attr('disabled','disabled');
-	                } else {
-	                    $('#form_errors').html(res.message).removeClass('hide');
-	                }
-	            }
-	        });
-	    } else {
-	        $('#form_errors').html('Please give a valid email address').removeClass('hide');
-	    }
-	    return false;
-	});
-
-
-
-
-
 });
